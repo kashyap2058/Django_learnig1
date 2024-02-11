99c83ba737eec33766ead8e2a956341d88e92d54
kashyap2058 <uzumakikashyap@gmail.com>
2024-02-11 16:06:24 +0545

    Project Commit

--- classbased_app/urls.py
+++ classbased_app/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('',HomeView.as_view(),name='classbased_home'),
+    path('classroom/',ClassRoomView.as_view(),name='classbased_classroom'),
 ]
--- classbased_app/views.py
+++ classbased_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
+from myapp.models import ClassRoom,Student,StudentProfile
 # Create your views here.
 class HomeView(TemplateView):
     template_name="classbased/home.html"
+
+
+
+class ClassRoomView(ListView):
+    template_name='classbased/classroom_add.html'
+
+    #aagadi normally functional approach ma hamile tala return garda request,template name(html file),context load garya them
+    #yo context vitra query set pathako them tei garne yesma ni for which
+    queryset=ClassRoom.objects.all() #context ko value ko kaam garxa automatically
+    context_object_name="classrooms" #context ko key ko naam dine kaam garxa automatically
